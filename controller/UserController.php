be70a43bbdc70dd0f60f8ca366d6be1bf286b3c5
IkkoTokunaga <tokuppee1515@gmail.com>
2020-09-11 20:15:28 +0900

    不要なコメントを削除

--- controller/UserController.php
+++ controller/UserController.php
@@ -3,42 +3,15 @@
 
 class UserController {
 
-    // private $name;
-    // private $email;
-    // private $pass;
-    // private $re_pass;
-
-
-    // public function set($post)
-    // {
-    //     $this->name = $post['name'];
-    //     $this->email = $post['email'];
-    //     $this->pass = $post['pass'];
-    //     $this->re_pass = $post['re_pass'];
-
-    // }
-
-    // public function setMember($post)
-    // {
-    //     $this->name = $post['m_name'];
-    //     $this->email = $post['m_email'];
-    //     $this->pass = $post['m_pass'];
-    // }
-
     public function save()
     {
 
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
             ////postの値を変数に格納しておく
             $user_date = $_POST;
-            //validationを通す
             $validation = new UserValidation();
             $validation->setDate($user_date);
             $check_date = $validation->checkDate();
-            // $validation->setEmail($this->email);
-            // $check_email = $validation->checkEmail();
-            // $validation->setPass($this->pass);
-            // $check_pass = $validation->checkPass($this->re_pass);
             if(!$check_date){
                 $_SESSION['user_err'] = $validation->getErrMessage();
                 $params = sprintf("?name=%s&email=%s",$user_date['name'], $user_date['email']);
@@ -48,8 +21,6 @@ class UserController {
 
             //validationを通ったものを使う
             $user = $validation->getDate();
-            // $user['email'] = $validation->getEmail();
-            // $user['pass'] =  $validation->getPass();
 
             //DBへ登録
             $new_member = new User();
